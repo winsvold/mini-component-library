b5e1a0e6e37674955bacd6e7d55be003b0b95aaf
winsvold <29202094+winsvold@users.noreply.github.com>
2021-04-22 22:44:10 +0200

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,54 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Base = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  &:after {
+    content: '';
+    display: block;
+    height: 100%;
+    width: 100%;
+    background-color: ${COLORS.primary};
+    border-radius: 4px;
+    clip-path: polygon(0 0, ${p => p.value}% 0, ${p => p.value}% 100%, 0 100%);
+  }
+`;
+
+const Large = styled(Base)`
+  height: 24px;
+  padding: 4px;
+  border-radius: 8px;
+`;
+
+const Medium = styled(Base)`
+  height: 12px;
+  border-radius: 4px;
+`;
+
+const Small = styled(Base)`
+  height: 8px;
+  border-radius: 4px;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+  let Variant;
+  switch (size) {
+    case 'small':
+      Variant = Small;
+      break;
+    case 'large':
+      Variant = Large;
+      break;
+    default:
+      Variant = Medium;
+  }
+
+  return <>
+    <Variant value={value} />
+    <VisuallyHidden><progress value={value / 100}/></VisuallyHidden>
+  </>;
 };
 
 export default ProgressBar;
