0b14c1a501fb000772f82f425d2e9ee2ec816855
winsvold <29202094+winsvold@users.noreply.github.com>
2021-04-23 08:41:24 +0200

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
+import {COLORS} from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import {getDisplayedValue} from './Select.helpers';
+import {ChevronDown} from "react-feather";
 
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+const Wrapper = styled.div`
+  display: inline-flex;
+  position: relative;
+  align-items: center;
+`;
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+const Style = styled.select`
+  width: fit-content;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  padding: 12px 52px 12px 12px;
+  color: ${COLORS.gray700};
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const StyledChevronDown = styled(ChevronDown)`
+  position: absolute;
+  right: 8px;
+  pointer-events: none;
+  width: 16px;
+  stroke: ${COLORS.gray700};
+
+  ${Style}:hover + & {
+    stroke: ${COLORS.black};
+  }
+`;
+
+const Select = ({label, value, onChange, children}) => {
+    const displayedValue = getDisplayedValue(value, children);
+
+    return (
+        <Wrapper>
+            <Style value={value} onChange={onChange}>
+                {children}
+            </Style>
+            <StyledChevronDown/>
+        </Wrapper>
+    );
 };
 
 export default Select;
